46f49d147ecd185265be5a6c2df4cf8be56b5988
149203 <149203@gmail.com>
2019-04-29 10:15:31 -0700

    intial commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# WUT?
+
+This is a starter to build quick prototypes with React and Bootstrap 4 (via SCSS).
 
 ## Available Scripts
 
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Main() {
+   return (
+      <div className="container">
+         <div className="row">
+            <div className="col-12 mt-3">
+               <h1>Hello world</h1>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default Main
